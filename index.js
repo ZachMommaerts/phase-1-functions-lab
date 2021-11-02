a0771bd6772f1f3136f1da16b35f24d8b0896b80
Zach <zmommaerts@gmail.com>
2021-11-01 19:57:09 -0500

    commit tree 39caf3c73f9ceb37d5c1e0542e4061349cb23d35

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(number) {
+    let numberOfBlocks;
+
+    if (number >= 42) {
+        numberOfBlocks = number - 42;
+    } else if (number < 42) {
+        numberOfBlocks = 42 - number;
+    };
+
+    return numberOfBlocks;
+}
+
+function distanceFromHqInFeet(number) {
+    let numberOfFeet = number;
+
+    if (number >= 42) {
+        numberOfFeet = number - 42;
+    } else if (number < 42) {
+        numberOfFeet = 42 - number;
+    };
+
+    return numberOfFeet * 264;
+}
+
+function distanceTravelledInFeet(num1, num2) {
+    let feetTravelled;
+    { num1 >= num2 ? feetTravelled = (num1 - num2) * 264 : feetTravelled = (num2 - num1) * 264};
+    return feetTravelled;
+}
+
+function calculatesFarePrice(num1, num2) {
+    let fare;
+    let feetTravelled = distanceTravelledInFeet(num1, num2);
+
+    if(feetTravelled <= 400) {
+        fare = 0;
+    } else if (feetTravelled <= 2000) {
+        fare = (feetTravelled - 400) * .02;
+    } else if (feetTravelled <= 2500) {
+        fare = 25;
+    } else {
+        fare = 'cannot travel that far';
+    }
+
+    return fare;
+}
+
